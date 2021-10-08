b02c1778fa8e690914749a78b9c2af428eabb044
SunayBhat1 <56206141+SunayBhat1@users.noreply.github.com>
2021-10-08 16:19:16 -0700

    Finished Basic MAB Article Code (Part 2)

---
+++ Fantasy RL Series/ImportLeague.py
@@ -0,0 +1,4 @@
+from espn_api.football import League # https://github.com/cwendt94/espn-api/wiki
+
+def getLeague():
+    return League(league_id=1874609, year=2021, espn_s2='AEBkLb6ZRC0Caq5MVHVRxpx4PRuJMfplVRuBdGbMe67yUmT2SFdxaqIeXN1gCa83PZMiRvib7EzNOKyfR4iZ01LR7bfRgg1bCcM9ycnwO6b10%2FOOdMxMdFhsB2DDQXIsjh7Pi4jB0gUkT9e2bi%2B3KEOOmYUuKjE6EYeiKXc59Pf40LlQ9%2BASfIhO6uNP%2FQ5dfB97t6Jt41lfYKzoyTZ7bZJ1RW8TzfUTzUK47%2BPoe53Va157DqaDbhbF725ZKIjpFdXWjMw5ziGwuWfloKwGLV963bUye9TXkYFAnrmpvbR1VA%3D%3D', swid='{EA6617AF-C6C9-491F-B5D6-5870D69E957B}')
